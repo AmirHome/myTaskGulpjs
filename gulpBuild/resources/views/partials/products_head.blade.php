ca301f0ae79d9a8aa9d523c448ffff3b247969a4
Amir <amir.email@yahoo.com>
2016-01-12 15:06:24 +0200

    clean dist

--- gulpBuild/resources/views/partials/products_head.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- partials products head -->
-{!! HTML::style('resources/assets/css/accordion.css') !!}
-{!! HTML::style('resources/assets/css/imghover/jquery.hoverGrid.css') !!}
-
-
-<script src="http://thecodeplayer.com/uploads/js/prefixfree-1.0.7.js" type="text/javascript" type="text/javascript"></script>
-
-
